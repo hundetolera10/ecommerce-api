6d3376eab1d769c8765f2abc6901e04a1e14e224
Hunde Tolera <hundetolera225@gmail>
2025-12-13 09:27:48 +0300

    Week 2: Add category and product models with CRUD APIs

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
+from .views import (
+    CategoryListCreateView, CategoryDetailView,
+    ProductListCreateView, ProductDetailView
+)
+urlpatterns = [
+     # Categories
+    path('categories/', CategoryListCreateView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailView.as_view()),
 
-urlpatterns = []
+    # Products
+    path('', ProductListCreateView.as_view()),
+    path('<int:pk>/', ProductDetailView.as_view()),
+]
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+# ----------------- CATEGORY ------------------
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    def get_permissions(self):
+        if self.request.method in ["PUT", "DELETE"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+# ----------------- PRODUCT ------------------
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # Allow anyone to list, but only admin can create
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "DELETE"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]

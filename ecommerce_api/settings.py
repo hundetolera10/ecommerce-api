1e87871b2d40b6387d7a2848a49b29a6a39ba07d
Hunde <hundetolera225@gmail.com>
2026-01-16 16:50:59 +0300

    Remove secret key from settings.py
    Removed the secret key from settings for security reasons.

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8iw&l3ghvc0$$2b0kp9881%w3^$z0gpg(8ghbty5*iq0c!-a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
